2181283d9436f02f8a7f5dd92302883c210f3a1d
ritotombe <ritotombe@gmail.com>
2020-08-23 13:52:54 +1000

    validasi

--- application/controllers/Antri.php
+++ application/controllers/Antri.php
@@ -162,12 +162,13 @@ class Antri extends REST_Controller
                 }
 
                 /* cek apakah peserta sudah terdaftar sebelumnya */
-
-                $noantrian = $this->antrian->cek_terdaftar_ref($nomorkartu, $nik, $poli[0]->id_poliklinik, $nomorreferensi);
-                $ceknoantrian = $this->check($noantrian);
-                if ($ceknoantrian->status === true) {
-                    $this->gagal('Nomor referensi anda sudah terdaftar antrian.');
-                    exit();
+                if($nomorreferensi && $nomorreferensi!=""){
+                    $noantrian = $this->antrian->cek_terdaftar_ref($nomorkartu, $nik, $poli[0]->id_poliklinik, $nomorreferensi);
+                    $ceknoantrian = $this->check($noantrian);
+                    if ($ceknoantrian->status === true) {
+                        $this->gagal('Nomor referensi anda sudah terdaftar antrian.');
+                        exit();
+                    }
                 }
 
                 $noantrian = $this->antrian->cek_terdaftar($nomorkartu, $nik, $poli[0]->id_poliklinik, $tanggalperiksa);
